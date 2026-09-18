d795803f5bb1be007972eb4f1836eb277706171f
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2026-09-18 19:40:24 +0530

    Update 10-simple-calculator.cpp

--- 01-cpp-basics/02-conditions/10-simple-calculator.cpp
+++ 01-cpp-basics/02-conditions/10-simple-calculator.cpp
@@ -1,1 +1,35 @@
 
+#include <iostream>
+using namespace std;
+int main() {
+    int first,second,opretor;
+    cout << "'+' Please choose 1 \n'-' Please choose 2 \n'*' Please choose 3 \n'/' Please choose 4" <<endl;
+    cout << "please choose your Opretor :" << endl;
+    cin >> opretor;
+    cout << "Please Enter your First Number: " <<endl;
+    cin >> first;
+    cout << "Please Enter your Second Number: " <<endl;
+    cin >> second;
+    float calculat;
+    switch(opretor){
+        case 1:
+            calculat = first + second;
+            cout << first << " + " << second << " = " <<calculat << endl;
+            break;
+        case 2:
+            calculat = first - second;
+            cout << first << " - " << second << " = " <<calculat << endl;
+            break;
+        case 3:
+            calculat = first * second;
+            cout << first << " * " << second << " = " <<calculat << endl;
+            break;
+        case 4:
+            calculat = first / second;
+            cout << first << " / " << second << " = " <<calculat << endl;
+            break;
+        default:
+            cout << "Please Enter Vaild Opretor: " <<endl;
+    }
+    return 0;
+}
