8f93efdc661e32b26021bfdfd60f2c523e4e397b
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2026-09-18 18:54:55 +0530

    Add even-odd check for user input number

--- 01-cpp-basics/02-conditions/07-even-odd.cpp
+++ 01-cpp-basics/02-conditions/07-even-odd.cpp
@@ -1,1 +1,16 @@
 
+#include <iostream>
+using namespace std;
+int main() {
+    int number;
+    cout << "Enter Your Number: " << endl;
+    cin >> number;
+    if(number % 2 == 0){
+        cout << "Number is Even" << endl;
+    }
+        
+    else {
+        cout << "Number is Odd" << endl;
+    }
+    return 0;
+}
