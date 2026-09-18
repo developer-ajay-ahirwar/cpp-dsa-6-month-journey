5090ab3c3725fb83ca6899a37c4c9ae3f0260d96
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2026-09-18 19:14:22 +0530

    Update 08-largest-of-three.cpp

--- 01-cpp-basics/02-conditions/08-largest-of-three.cpp
+++ 01-cpp-basics/02-conditions/08-largest-of-three.cpp
@@ -1,1 +1,42 @@
 
+#include <iostream>
+using namespace std;
+int main() {
+    int firstNumber,secondNumber,thridNumber;
+    cout << "Enter Your First Number: " << endl;
+    cin >> firstNumber;
+    cout << "Enter Your Second Number: " << endl;
+    cin >> secondNumber;
+    cout << "Enter Your Thrid Number: " << endl;
+    cin >> thridNumber;
+    
+    if (firstNumber > secondNumber){
+        if (firstNumber > thridNumber){
+            cout << "First Number is the Largest Number:";
+        }
+        else {
+            cout << "First Number";
+        }
+    }
+    else if (secondNumber > thridNumber){
+        if(secondNumber > firstNumber){
+            cout << "Second Number is the Largest Number:";
+        }
+        else {
+             cout << "Second Number";
+        }
+    }
+    else if (thridNumber > firstNumber){
+        if (thridNumber > secondNumber){
+            cout << "Thrid Number is the Largest Number:";
+        }
+        else {
+            cout << "Thrid Number;";
+        }
+    }
+    else {
+        cout << "Thrid Number is the Largest Number:";
+    }
+    
+    return 0;
+}
