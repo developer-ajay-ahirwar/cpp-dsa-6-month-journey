c7407962d0413561ee1d1b2535bfdd162619aac7
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2026-09-18 18:48:52 +0530

    Update 06-positive-negative.cpp

--- 01-cpp-basics/02-conditions/06-positive-negative.cpp
+++ 01-cpp-basics/02-conditions/06-positive-negative.cpp
@@ -1,1 +1,22 @@
+// Problem: Check whether a number is positive, negative, or zero.
+// Topic: if-else-if-else
+// Difficulty: Easy
+// Approach: Take a number as input and check its value using conditional statements.
 
+#include <iostream>
+using namespace std;
+int main() {
+    int number;
+    cout << "Enter Your Number: " << endl;
+    cin >> number;
+    if (number > 0){
+        cout << "Your Number is Positive: " <<endl;
+    }
+    else if(number < 0){
+        cout << "Your Number is Negative: " <<endl;
+    }
+    else {
+        cout << "Your Number is Zero: " << endl;
+    }
+    return 0;
+}
