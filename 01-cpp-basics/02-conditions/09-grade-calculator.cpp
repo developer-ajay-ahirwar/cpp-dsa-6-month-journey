7b6c598f4e6f880c3b193e7c1178b3c27bf873ea
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2026-09-18 19:23:45 +0530

    Update 09-grade-calculator.cpp

--- 01-cpp-basics/02-conditions/09-grade-calculator.cpp
+++ 01-cpp-basics/02-conditions/09-grade-calculator.cpp
@@ -1,1 +1,29 @@
 
+#include <iostream>
+using namespace std;
+int main() {
+    float percentage;
+    cout << "Enter Your Percentage : " << endl;
+    cin >> percentage;
+    
+    if(percentage > 101 || percentage < 0){
+        cout << "Please Enter your Percentage 1 to 100 only";
+    }
+    else if (percentage >= 90){
+        cout << "Your A Grade ";
+    }
+    else if (percentage >=75){
+        cout << "Your B Grade ";        
+    }
+    else if (percentage >=60){
+        cout << "Your C Grade ";
+    }
+    else if (percentage >= 40){
+        cout << "Your D Grade ";
+    }
+    else {
+        cout << "You are the Fail";
+    }
+    
+    return 0;
+}
