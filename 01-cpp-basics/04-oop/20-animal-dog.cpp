d8ae2101f759d19a558ac76218dff0712bb340e3
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2026-09-20 15:55:17 +0530

    Update 20-animal-dog.cpp

--- 01-cpp-basics/04-oop/20-animal-dog.cpp
+++ 01-cpp-basics/04-oop/20-animal-dog.cpp
@@ -1,1 +1,28 @@
+// Problem: Create an Animal class and inherit it into a Dog class.
+// Topic: Inheritance, Class, Object, Member Functions
+// Difficulty: Easy
+// Approach: Create an Animal class with an eat function and inherit it into Dog with an additional bark function.
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal{
+    public:
+    void eat(){
+        cout << "eat:" << endl;
+    }
+
+};
+
+class Dog : public Animal{
+    public:
+    void bark(){
+        cout << "Dog bark: " << endl;
+    }
+};
+int main(){
+    Dog d1;
+    d1.eat();
+    d1.bark();
+}
