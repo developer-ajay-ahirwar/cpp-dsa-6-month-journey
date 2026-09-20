a1a5c29e58d43d5f1c53010b8f1c09594bc5cbe6
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2026-09-20 15:54:14 +0530

    Update 19-employee.cpp

--- 01-cpp-basics/04-oop/19-employee.cpp
+++ 01-cpp-basics/04-oop/19-employee.cpp
@@ -1,1 +1,31 @@
+// Problem: Create an Employee class to store and display employee information.
+// Topic: Class, Object, Constructor
+// Difficulty: Easy
+// Approach: Create an Employee class and initialize employee details using a constructor.
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Employee{
+    public:
+    string name;
+    float salary;
+    string department;
+
+    
+    void desplay(){
+        cout << "Name: " << name << "\nSalary: " << salary << "\nDepartment: " << department << endl;  
+    }
+    Employee(string nameE,float salaryE,string deptE){
+        name = nameE;
+        salary = salaryE;
+        department = deptE;
+    }
+};
+int main(){
+    Employee e1("Raj",25000,"Computer dept");
+    e1.desplay();
+
+    return 0;
+}
