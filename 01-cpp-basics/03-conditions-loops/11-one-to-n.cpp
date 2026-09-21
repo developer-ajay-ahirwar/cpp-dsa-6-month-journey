61dcc708f5228453c124a8cf7d042fda5a84a185
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2026-09-21 23:56:42 +0530

    Update 11-one-to-n.cpp

--- 01-cpp-basics/03-conditions-loops/11-one-to-n.cpp
+++ 01-cpp-basics/03-conditions-loops/11-one-to-n.cpp
@@ -1,1 +1,16 @@
 
+#include <iostream>
+using namespace std;
+
+int main(){
+    int Number;
+    cout << "Enter a number: ";
+    cin >> Number;
+    int total = 0;
+    for(int i = 1; i <= Number; i++){
+        total += i;
+        
+    }
+    cout << total << " ";
+    return 0;
+}
