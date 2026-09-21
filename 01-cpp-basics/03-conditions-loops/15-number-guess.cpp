4bf4bd71c04ed099a223ae0b22dcc946f46b790d
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2026-09-22 00:02:29 +0530

    Update 15-number-guess.cpp

--- 01-cpp-basics/03-conditions-loops/15-number-guess.cpp
+++ 01-cpp-basics/03-conditions-loops/15-number-guess.cpp
@@ -1,1 +1,16 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int secretNumber = 7;
+    int guess;
+    cout << "Guess the number (1-10): ";
+    cin >> guess;
+    if (guess == secretNumber) {
+        cout << "Congratulations! You guessed it right." << endl;
+    } else {
+        cout << "Sorry, that's not the correct number." << endl;
+    }
+    return 0;
+}
