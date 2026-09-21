71c8fbe4e83820c77cba5eeddd4869f5073116ad
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2026-09-22 00:00:21 +0530

    Update 13-even-numbers.cpp

--- 01-cpp-basics/03-conditions-loops/13-even-numbers.cpp
+++ 01-cpp-basics/03-conditions-loops/13-even-numbers.cpp
@@ -1,1 +1,17 @@
 
+#include <iostream>
+using namespace std;
+
+int main(){
+    int Number;
+    cout << "Enter a number: ";
+    cin >> Number;
+    int total = 0;
+    for(int i = 1; i <= Number; i++){
+        if (i % 2 == 0){
+            total += i;
+        }
+    }
+    cout << "The sum of even numbers from 1 to " << Number << " is: " << total << endl;
+    return 0;
+}
