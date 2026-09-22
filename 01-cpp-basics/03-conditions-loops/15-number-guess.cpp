3c7dcd0ae7522554cd8e710a4c43325c729cb4ce
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2026-09-22 22:13:06 +0530

    Update 15-number-guess.cpp

--- 01-cpp-basics/03-conditions-loops/15-number-guess.cpp
+++ 01-cpp-basics/03-conditions-loops/15-number-guess.cpp
@@ -1,3 +1,7 @@
+// Problem: Create a number guessing game with a fixed secret number.
+// Topic: while loop, if-else
+// Difficulty: Intermediate
+// Approach: Repeatedly take guesses and compare them with the secret number until the correct number is guessed.
 
 #include <iostream>
 using namespace std;
