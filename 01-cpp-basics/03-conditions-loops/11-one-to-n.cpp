ebdf881e7c722f815ec81602aa5870e4643dbd0c
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2026-09-22 22:11:34 +0530

    Update 11-one-to-n.cpp

--- 01-cpp-basics/03-conditions-loops/11-one-to-n.cpp
+++ 01-cpp-basics/03-conditions-loops/11-one-to-n.cpp
@@ -1,3 +1,7 @@
+// Problem: Print numbers from 1 to N.
+// Topic: for loop
+// Difficulty: Easy
+// Approach: Use a for loop to print every number starting from 1 up to N.
 
 #include <iostream>
 using namespace std;
