93aaa6c83ac513afcf84da16bba08860cd2d3cef
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2026-09-23 10:39:48 +0530

    Update 08-leap-year.cpp

--- 02-conditions/02-medium/08-leap-year.cpp
+++ 02-conditions/02-medium/08-leap-year.cpp
@@ -1,1 +1,26 @@
+// Problem: Check whether a given year is a leap year.
+// Topic: if-else, Logical Operators, Modulus Operator
+// Difficulty: Medium
+// Approach: Check whether the year is divisible by 400 or divisible by 4 but not by 100.
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int year;
+
+    cout << "Enter The Year: ";
+    cin >> year;
+
+    if (year % 400 == 0) {
+        cout << "Leap Year";
+    }
+    else if (year % 4 == 0 && year % 100 != 0) {
+        cout << "Leap Year";
+    }
+    else {
+        cout << "Not Leap Year";
+    }
+
+    return 0;
+}
