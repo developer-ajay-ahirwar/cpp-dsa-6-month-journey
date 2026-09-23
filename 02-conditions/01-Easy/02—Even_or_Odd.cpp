d1ed8fa69ce8cc7b24e3550948ae023388cff1b0
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2026-09-23 09:04:51 +0530

    Update 02—Even_or_Odd.cpp

--- 02-conditions/01-Easy/02—Even_or_Odd.cpp
+++ 02-conditions/01-Easy/02—Even_or_Odd.cpp
@@ -1,1 +1,20 @@
+// Problem: Check whether a given number is even or odd.
+// Topic: if-else, Modulus Operator
+// Difficulty: Easy
+// Approach: Check the remainder when the number is divided by 2.
 
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter your Number: " << endl;
+    cin >> n;
+    if(n % 2 ==0 ){
+        cout << "Number " << n << " is Even";
+    }
+    else {
+        cout << "Number N is Odd";
+    }
+    return 0;
+}
