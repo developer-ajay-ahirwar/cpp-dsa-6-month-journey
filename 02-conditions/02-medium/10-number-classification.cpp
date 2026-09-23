b01d47d300fecec57d9408a1b8dc2695163f195d
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2026-09-23 23:46:16 +0530

    Solve the Number Classification problem
    Classifies the input number as positive even, positive odd, negative even, negative odd, or zero based on user input.

--- 02-conditions/02-medium/10-number-classification.cpp
+++ 02-conditions/02-medium/10-number-classification.cpp
@@ -1,1 +1,33 @@
+// Problem: Classify a number as positive even, positive odd, negative even, negative odd, or zero.
+// Topic: Nested if, Modulus Operator
+// Difficulty: Medium
+// Approach: First check the sign of the number and then check whether it is even or odd.
 
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number;
+    cout << "Enter Your Number: ";
+    cin >> number;
+
+    if(number > 0){
+        if(number % 2 == 0){
+            cout << "Positive Even";
+        }
+        else {
+            cout << "Positive Odd";
+        }
+    }
+    else if(number < 0){
+        if(number % 2 == 0){
+            cout << "Negative Even";
+        }
+        else {
+            cout << "Negative Odd";
+        }
+    }
+    else {
+        cout << "Number is Zero";
+    }
+}
