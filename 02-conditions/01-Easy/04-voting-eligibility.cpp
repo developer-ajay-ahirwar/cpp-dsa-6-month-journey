654a7390ea4ab2e07cda0810ba15101aeb04d8bb
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2026-09-23 09:17:06 +0530

    Update 04-voting-eligibility.cpp

--- 02-conditions/01-Easy/04-voting-eligibility.cpp
+++ 02-conditions/01-Easy/04-voting-eligibility.cpp
@@ -1,1 +1,20 @@
+// Problem: Check whether a person is eligible to vote based on age.
+// Topic: if-else
+// Difficulty: Easy
+// Approach: Compare the given age with the minimum voting age.
 
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout << "Enter your Age: ";
+    cin >> age;
+    if(age >= 18){
+        cout << "You are Eligibil to put in your Vote";
+    }
+    else {
+        cout << "You are Not Eligibil to put in your Vote";
+    }
+    return 0;
+}
