d83c7b52c88c5912e3e08c7f62dc014b4470e67f
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2026-09-23 09:12:43 +0530

    Update 03-largest-two.cpp

--- 02-conditions/01-Easy/03-largest-two.cpp
+++ 02-conditions/01-Easy/03-largest-two.cpp
@@ -1,1 +1,25 @@
+// Problem: Find the largest between two numbers.
+// Topic: if-else
+// Difficulty: Easy
+// Approach: Compare the two numbers using an if-else condition.
 
+#include <iostream>
+using namespace std;
+int main(){
+    int first,second;
+    cout << "Enter your First Number: ";
+    cin >> first;
+    cout << "Enter Your Second Number: ";
+    cin >> second;
+
+    if (first == second){
+        cout << "Both Number are Equal";
+    }
+    else if(first > second){
+        cout << "First Number is Largest";
+    }
+    else{
+        cout << "Second Number is Largest";
+    }
+    return 0;
+}
