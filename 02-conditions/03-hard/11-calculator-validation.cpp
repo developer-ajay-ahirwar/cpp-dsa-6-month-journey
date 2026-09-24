8f08114b7e0f5820d5c87cdf98f1cf56ee3b4ba2
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2026-09-24 09:28:30 +0530

    Solve the Calculator-Validation

--- 02-conditions/03-hard/11-calculator-validation.cpp
+++ 02-conditions/03-hard/11-calculator-validation.cpp
@@ -1,1 +1,45 @@
+// Problem: Perform arithmetic operations while handling invalid operators and division by zero.
+// Topic: switch, if-else
+// Difficulty: Hard
+// Approach: Use switch for operations and conditional checks for invalid cases.
 
+#include <iostream>
+using namespace std;
+
+int main(){
+    int first,second;
+    char operators;
+    cout << "Enter your First Number: ";
+    cin >> first;
+    cout << "Enter your second Number: ";
+    cin >> second;
+    cout << "Enter your operator: ";
+    cin >> operators;
+    float calculatingValue = 0;
+    switch(operators){
+        case '+':
+            calculatingValue = first + second;
+                cout << calculatingValue << " Your Calculting Value";
+            break;
+        case '-':
+            calculatingValue = first - second;
+                cout << calculatingValue << " Your Calculting Value";
+            break;
+        case '*':
+            calculatingValue = first * second;
+                cout << calculatingValue << " Your Calculting Value";
+            break;
+        case '/':
+            if(second != 0){
+                calculatingValue = (float)first / second;
+                cout << calculatingValue << " Your Calculting Value";
+            }
+            else {
+                cout << "Not division by zero";
+            }
+            break;
+        default:
+            cout << operators << " Your Opertor Invalid";
+            break;
+    }
+}
