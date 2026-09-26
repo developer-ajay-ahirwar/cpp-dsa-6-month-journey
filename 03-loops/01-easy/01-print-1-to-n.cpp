5098d885b7818dddb2b8bfc532c0e7d08425b660
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2026-09-26 22:16:30 +0530

    Solve the Print all numbers from 1 to N.

--- 03-loops/01-easy/01-print-1-to-n.cpp
+++ 03-loops/01-easy/01-print-1-to-n.cpp
@@ -1,1 +1,17 @@
+// Problem: Print all numbers from 1 to N.
+// Topic: for loop
+// Difficulty: Easy
+// Approach: Use a for loop starting from 1 and continue until N.
 
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cout<< "Enter Number of N: ";
+	cin >> n;
+	for(int i = 0;i < n;i++){
+		cout<< i<< endl;
+	}
+	return 0;
+}
