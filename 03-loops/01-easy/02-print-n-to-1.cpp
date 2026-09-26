c901b58bcdca68420be9f1db9804306c3f229258
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2026-09-26 22:23:13 +0530

    Solve the Print all numbers from N to 1 in reverse order

--- 03-loops/01-easy/02-print-n-to-1.cpp
+++ 03-loops/01-easy/02-print-n-to-1.cpp
@@ -1,1 +1,19 @@
+// Problem: Print all numbers from N to 1 in reverse order.
+// Topic: for loop
+// Difficulty: Easy
+// Approach: Start the loop from N and decrease the value until it reaches 1.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cout<<"Enter Number ";
+	cin>> n;
+	for(int i = n;i>0;i--){
+		cout<<i <<endl;
+  }
+  return 0;
+
+}
 
